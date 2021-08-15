525af3fe0c41db51cb7d82df16431202100b2bf9
murilossilva <82983123+murilossilva@users.noreply.github.com>
2021-08-15 02:35:26 -0300

    v1.0.4 - adiciona equação de segundo grau

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@ import math as m
 
 print('1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão')
 print('5 - Potenciação\n6 - Raiz quadrada\n7 - Raiz Cúbica\n8 - Logaritmo')
-print('0 - Sair\n')
+print('9 - Equação de 2º Grau\n0 - Sair\n')
 menu = int(input("Digite a operação desejada: "))
 print('')
 
@@ -21,11 +21,33 @@ def novamente(x):
         else:
             print('Informe uma opção válida')
 
+#Equação do 2º grau
+def equacao():
+    a_dig = float(input('Digite o valor de a: '))
+    b_dig = float(input('Digite o valor de b: '))
+    c_dig = float(input('Digite o valor de c: '))
+    print_raiz(a_dig, b_dig, c_dig)
+    
+def delta(a,b,c):
+    return b ** 2 - 4 * a * c
+
+def print_raiz(a,b,c):
+    d = delta(a,b,c)
+    if d == 0:
+        raiz1 = (-b)/(2*a)
+    else:
+        if d < 0:
+            print('Esta equação não possuí raizes reais')
+        else:
+            raiz1 = (-b - m.sqrt(d)) / (2 * a)
+            raiz2 = (-b + m.sqrt(d)) / (2 * a)
+            print('\nA primeira raiz é', raiz1)
+            print('A segunda raiz é', raiz2)
 if menu == 0:
     print('Tudo bem, estarei te aguardando!')
 
 while menu != 0:
-    if menu not in range(0,9):
+    if menu not in range(0,10):
         print('Eiiiii! Essa opção não existe, tente novamente!\n')
         menu = int(input('Digite a operação desejada: '))
     #soma        
@@ -84,5 +106,10 @@ while menu != 0:
         n = float(input('Digite o valor do logaritmando: '))
         base = float(input('Digite o valor da base: '))
         print('O resultado do logaritmo de {} na base {} = {}\n'.format(n, base, (m.log(n)/m.log(base))))
+        if novamente(x) == False:
+            break
+    #equação de 2º grau
+    if menu == 9:
+        equacao()
         if novamente(x) == False:
             break
