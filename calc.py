2f9037d5d1bafa81234e0c15b3ad69bd6de7a9e6
murilossilva <murilossilva@usp.br>
2021-09-15 13:14:53 -0300

    exclui chamadas repetitivas

--- calc.py
+++ calc.py
@@ -6,20 +6,17 @@ print('9 - Equação de 2º Grau\n0 - Sair\n')
 menu = int(input("Digite a operação desejada: "))
 print('')
 
-x = 1
-
 #Função para realizar novamente o uso da calculadora#
-
-def novamente(x):
-    while x == 1:
-        r = str(input('Gostaria de continuar?[s/n] '))
-        if r in 'nN':
-            print('Obrigado por confiar nos meus cálculos, volte logo!')
-            return False
-        if r in 'sS':
-            return True
-        else:
-            print('Informe uma opção válida')
+def novamente():
+    r = str(input('Gostaria de continuar?[s/n] '))
+    if r in 'nN':
+        print('Obrigado por confiar nos meus cálculos, volte logo!')
+        return False
+    if r in 'sS':
+        return True
+    else:
+        print('Informe uma opção válida')
+        return novamente()
 
 #Equação do 2º grau
 def equacao():
@@ -55,22 +52,19 @@ while menu != 0:
         n = float(input('Digite o primeiro valor a ser somado: '))
         m = float(input('Digite o segundo valor a ser somado: '))
         print('O resultado de {} + {} = {}\n'.format(n, m, round(float(n) + float(m), 2)))
-        if novamente(x) == False:
-            break
+        
     #subtração
     if menu == 2:
         n = float(input('Digite o primeiro valor a ser subtraído: '))
         m = float(input('Digite o segundo valor a ser subtraído: '))
         print('O resultado de {} - {} = {}\n'.format(n, m, round(float(n) - float(m), 2)))
-        if novamente(x) == False:
-            break
+ 
     #multiplicação    
     if menu == 3:
         n = float(input('Digite o primeiro valor a ser multiplicado: '))
         m = float(input('Digite o segundo valor a ser multiplicado: '))
         print('O resultado de {} * {} = {}\n'.format(n, m, round(float(n) * float(m), 2)))
-        if novamente(x) == False:
-            break
+ 
     #divisão    
     if menu == 4:
         n = float(input('Digite o dividendo: '))
@@ -80,36 +74,32 @@ while menu != 0:
         #regra para o divisor diferente de 0
         else:
             print('O divisor não pode ser nulo, tente novamente!')
-        if novamente(x) == False:
-            break
+
     #potenciação    
     if menu == 5:
         n = float(input('Digite o valor da base: '))
         m = float(input('Digite o valor do expoente: '))
         print('O resultado de {}^{} = {}\n'.format(n, m, round(float(n) ** float(m), 2)))
-        if novamente(x) == False:
-            break
+
     #raiz quadrada
     if menu == 6:
         n = float(input('Digite o valor do radicando: '))
         print('A raiz quadrada de {} é = {}\n'.format(n, float(m.sqrt(n))))
-        if novamente(x) == False:
-            break
+
     #raiz cúbica
     if menu == 7:
         n = float(input('Digite o valor do radicando: '))
-        print('A raiz quadrada de {} é = {}\n'.format(n, float(n) ** (1/3)))
-        if novamente(x) == False:
-            break
+        print('A raiz cúbica de {} é = {}\n'.format(n, float(n) ** (1/3)))
+    
     #logaritmo
     if menu == 8:
         n = float(input('Digite o valor do logaritmando: '))
         base = float(input('Digite o valor da base: '))
         print('O resultado do logaritmo de {} na base {} = {}\n'.format(n, base, (m.log(n)/m.log(base))))
-        if novamente(x) == False:
-            break
+    
     #equação de 2º grau
     if menu == 9:
         equacao()
-        if novamente(x) == False:
-            break
+
+    if novamente() == False:
+        break
